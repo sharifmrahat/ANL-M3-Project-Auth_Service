f47c31cd645613a6497330faeca1b96a88aa11d3
sharifmrahat <sharifmrahat@gmail.com>
2023-06-01 20:35:00 +0600

    husky and lint-staged setup done

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express, { Application } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express, { Application } from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-const app: Application = express();
+const app: Application = express()
 
 //cors
-app.use(cors());
+app.use(cors())
 
 //dotenv
-dotenv.config();
+dotenv.config()
 
 //parse data
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //api-endpoints
-export default app;
+export default app
