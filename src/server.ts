5d16f499f76069057b13fdfe6021cfabf2f87170
sharifmrahat <sharifmrahat@gmail.com>
2023-06-03 15:53:29 +0600

    winston logger is added

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import app from './app'
 import mongoose from 'mongoose'
 import config from './config'
+import logger from './shared/logger'
 
 //database connection
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database is connected 🛢')
+    logger.info('Database is connected 🛢')
     app.listen(config.port, () => {
-      console.log(`Server listening on port ${config.port}`)
+      logger.info(`Server listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log(error)
+    logger.error(error)
   }
 }
 bootstrap()
