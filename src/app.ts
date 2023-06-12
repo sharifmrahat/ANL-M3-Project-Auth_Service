06b2573a0322caed3398aac4b6a92376b04a0f2d
sharifmrahat <sharifmrahat@gmail.com>
2023-06-12 23:52:23 +0600

    not found route error handler is added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import cors from 'cors'
-import express, { Application } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import routes from './app/routes'
+import httpStatus from 'http-status'
 const app: Application = express()
 
 app.use(cors())
@@ -14,5 +15,20 @@ app.use('/api/v1/', routes)
 
 //global error handler
 app.use(globalErrorHandler)
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  })
+  next()
+})
 
 export default app
