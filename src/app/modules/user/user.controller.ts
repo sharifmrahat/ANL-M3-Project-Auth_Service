09906aabeecf2eab732c1dce3266b24c233c3211
sharifmrahat <sharifmrahat@gmail.com>
2023-06-16 19:48:43 +0600

    user module is updated

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import { RequestHandler } from 'express-serve-static-core'
 import httpStatus from 'http-status'
 import catchAsync from '../../../shared/catchAsync'
@@ -7,17 +7,16 @@ import { IUser } from './user.interface'
 import { UserService } from './user.service'
 
 const createUser: RequestHandler = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response) => {
     const { user } = req.body
     const result = await UserService.createUser(user)
 
     sendResponse<IUser>(res, {
       statusCode: httpStatus.OK,
       success: true,
-      message: 'User created successfully!',
+      message: 'user created successfully!',
       data: result,
     })
-    next()
   }
 )
 
